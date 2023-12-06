678c995abb8ba0a806c8eb3c238e6e7db25d36ed
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-12-06 16:15:56 -0600

    Added data.txt file for advent data

---
+++ 06-December_6/puzzleOne.ts
@@ -0,0 +1,37 @@
+import fs from 'fs';
+
+// Define a custom type for an array of numbers
+type NumberArray = number[];
+
+// Define a custom interface for parsed data
+interface ParsedData {
+    times: NumberArray;
+    distances: NumberArray;
+}
+
+// Function to parse the data from the file
+const parseData = (filePath: string): ParsedData => {
+    const data = fs.readFileSync(filePath, 'utf-8').split('\r\n');
+    const times: NumberArray = data[0].split(":")[1].trim().replace(/\s\s+/g, ' ').split(" ").map(e => +e);
+    const distances: NumberArray = data[1].split(":")[1].trim().replace(/\s\s+/g, ' ').split(" ").map(e => +e);
+
+    return { times, distances };
+};
+
+// Read and parse the data
+const { times, distances } = parseData('./input.txt');
+
+let result = 1;
+
+for (let i = 0; i < times.length; i++) {
+    let count = 0;
+    for (let ms = 1; ms < times[i]; ms++) {
+        const dist = (times[i] - ms) * ms;
+        if (dist > distances[i]) {
+            count++;
+        }
+    }
+    result *= count;
+}
+
+console.log(result);
