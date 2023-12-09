67776291dd42225e72fa7f45814f572c7d063af7
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-12-09 14:59:05 -0600

    Added solution for puzzle one

--- 09-December_9/puzzleOne.ts
+++ 09-December_9/puzzleOne.ts
@@ -1,26 +1,53 @@
 import * as fs from 'fs';
 
-// Define a custom type for the array of numbers
-type NumberArray = number[];
+// Custom type for a sequence of numbers
+type Sequence = number[];
 
-const data: string[] = fs.readFileSync('./data.txt', 'utf-8').split('\r\n');
-
-let total: number = 0;
-
-for (const line of data) {
-    let dataLog: NumberArray = line.split(' ').map(e => +e);
-    const lastValues: NumberArray = [];
-
-    while (!dataLog.every(e => e === 0)) {
-        const nextArr: NumberArray = [];
-        for (let i = 1; i < dataLog.length; i++) {
-            nextArr.push(dataLog[i] - dataLog[i - 1]);
-        }
-        lastValues.push(dataLog[dataLog.length - 1]);
-        dataLog = nextArr;
-    }
-
-    total += lastValues.reduce((a, c) => a + c, 0);
+// Custom interface for a function that processes sequences
+interface SequenceProcessor {
+  (sequence: Sequence): Sequence[];
 }
 
-console.log(total);
+const getAllSequencesToZero: SequenceProcessor = (starSequence: Sequence): Sequence[] => {
+  let trackSequence: Sequence[] = [starSequence];
+  let currentSeq: Sequence = [...starSequence];
+  let nextSequence: Sequence = [];
+  while (!currentSeq.every(num => num === 0)) {
+    nextSequence = [];
+    for (let j = 0; j < currentSeq.length - 1; j++) {
+      if (j === currentSeq.length) {
+        break;
+      }
+      const diff = currentSeq[j + 1] - currentSeq[j];
+      nextSequence.push(diff);
+    }
+    trackSequence.push(nextSequence);
+    currentSeq = nextSequence;
+  }
+  return trackSequence;
+};
+
+const updateSequence = (trackSequence: Sequence[]): void => {
+  let seqVal = 0;
+  for (let j = trackSequence.length - 2; j >= 0; j--) {
+    const sequence = trackSequence[j];
+    const updatedVal = sequence[sequence.length - 1] + seqVal;
+    trackSequence[j].push(updatedVal);
+    seqVal = updatedVal;
+  }
+};
+
+const part1 = (file: string): number => {
+  const lines = fs.readFileSync(file, 'utf-8').split(/[\n\r]+/);
+  
+  let sum = 0;
+  for (let i = 0; i < lines.length; i++) {
+    const sequence: Sequence = lines[i].split(' ').map(num => Number(num));
+    let trackSequence: Sequence[] = getAllSequencesToZero(sequence);
+    updateSequence(trackSequence);
+    sum += trackSequence[0][trackSequence[0].length - 1];
+  }
+  return sum;
+};
+
+console.log(part1('input.txt'));
